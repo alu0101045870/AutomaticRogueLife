ba850d932904658f23b5b5f1b2dced40dfc9f84b
Fernando González <alu0101045870@ull.edu.es>
2019-01-03 10:29:00 +0000

    Mejoras de KB y percepciones previas a la implementación de bucle de juego

--- roguelike.pl
+++ roguelike.pl
@@ -3,37 +3,72 @@
 
 :- dynamic ([
        heroe_location/1,
-%       treasure_location/1,
+       trapdoor_location/1,
        npc_locations/1,
        monster_locations/1,
-%       visited/1,
-%       visited_cells/1,
+%       obst_locations/1,
+       visited/1,
+       visited_cells/1,
+       name/1,
+       gender/1,
        exp_gained/1,
        hp/1,
-       atk/1
+       atk/1,
+       npc_nearby/0
    ]).
+             % WS = world size, G = gender(0|1), CN = Character name
+start(WS, G, CN) :-
+    format('\t\tAUTOMATIC ROGUELIFE\n\n', []),
+    init_world(WS, G, CN),
 
-start(WS) :-
-    format('E A SPORTS ... CHENEGUEIN\n\n', []),
-    init_world(WS),
-    format('Escribe, crack', []).
+    introduction(G, CN).
+
+start :-
+    format('\t\tAUTOMATIC ROGUELIFE\n\n', []),
+    init_world(10, 0, "Hiro"),
+
+    introduction(0, "Hiro").
+
+
+
+introduction(G, CN) :-
+
+    ( G = 0 ->  format('~p ended up in a subterranean and got separated from his friends.
+It is his mission to find them and get out of here.\n
+For that he has to:\n\n
+    1. Find the key. Abby knows where it is.\n
+    2. Get a sword. Nero always has one with him (for some reason).\n
+    3. Get to the trapdoor. Margarette probably knows the way\n\n
+
+    Aaaand thats about it! You (player) do absolutely nothing.\n
+    But watch closely!' , [CN]);
+
+                format('~p ended up in a subterranean and got separated from her friends.
+It is her mission to find them and get out of here.\n
+For that she has to:\n\n
+    1. Find the key. Abby knows where it is.\n
+    2. Get a sword. Nero always has one with him (for some reason).\n
+    3. Get to the trapdoor. Margarette probably knows the way\n\n
+
+    Aaaand thats about it! You (player) do absolutely nothing.\n
+    But watch closely!\n\n\n' , [CN])
+    )
+
+
+    .
+
+
+
 
 %------------------------------------
 % Knowledge
 
-character(heroe).
-character(CH) :- lives(CH, la_laguna).
+character(CH) :- lives(CH, jospallad).
 npc(julian).
 npc(margarette).
 npc(nero).
 npc(abigail).
-npc(NPC) :- lives(NPC, la_laguna).
-
-where_is(NPC, L) :-
-    npc_locations([[NPC, L], [_,_], [_,_], [_,_]]);
-    npc_locations([[_,_], [NPC, L], [_,_], [_,_]]);
-    npc_locations([[_,_], [_,_], [NPC, L], [_,_]]);
-    npc_locations([[_,_], [_,_], [_,_], [NPC, L]]).
+npc(NPC) :- lives(NPC, jospallad).
 
 %------------------------------------
 % Perceptors
@@ -54,17 +89,67 @@ adjacent( [X1, Y1], [X2, Y2] ) :-
     ).
 
 isMonster(P) :-
-    monster_locations([P,_,_,_]);
-    monster_locations([_,P,_,_]);
-    monster_locations([_,_,P,_]);
-    monster_locations([_,_,_,P]).
+    monster_locations(P).
+
+where_is(NPC, L) :-
+    npc_locations([[NPC, L], [_,_], [_,_], [_,_]]);
+    npc_locations([[_,_], [NPC, L], [_,_], [_,_]]);
+    npc_locations([[_,_], [_,_], [NPC, L], [_,_]]);
+    npc_locations([[_,_], [_,_], [_,_], [NPC, L]]).
+
+see_campfire(yes) :-
+    heroe_location([X,Y]),
+    juli_near([X,Y]);
+    marg_near([X,Y]);
+    nero_near([X,Y]);
+    abby_near([X,Y]).
+
+see_campfire(no).
+
+juli_near(Loc) :-
+    npc_locations([[_, X],[_, _],[_, _],[_, _]]),
+    adjacent(X, Loc).
+
+marg_near(Loc) :-
+    npc_locations([[_, _],[_, X],[_, _],[_, _]]),
+    adjacent(X, Loc).
+
+nero_near(Loc) :-
+    npc_locations([[_, _],[_, _],[_, X],[_, _]]),
+    adjacent(X, Loc).
+
+abby_near(Loc) :-
+    npc_locations([[_, _],[_, _],[_, _],[_, X]]),
+    adjacent(X, Loc).
+
+        % Monsters smell... duh
+smelly(yes) :-
+    heroe_location(AL),
+    isMonster(ML),
+    adjacent(AL,ML).
+
+smelly(no).
+
+        % trapdoor = wind
+windy(yes) :-
+    heroe_location(AL),
+    trapdoor_location(TL),
+    adjacent(AL, TL).
+
+windy(no).
 
 %--------------------------------------------
 % Game setup
 
-init_agent :-
+init_agent(G, CN) :-
     retractall( heroe_location(_)),
     assert( heroe_location([1,1])),
+
+    retractall(gender(_)),
+    assert(gender(G)),
+
+    retractall(name(_)),
+    assert(name(CN)),
 
     retractall( exp_gained(_)),
     assert( exp_gained(0)),
@@ -82,7 +167,8 @@ init_npcs(WS) :-
     random_between(1, WS, X2), random_between(1, WS, Y2),
     random_between(1, WS, X3), random_between(1, WS, Y3),
 
-    assert( npc_locations([[julian, [1,1]], [margarette, [X1,Y1]], [nero,[X2,Y2]], [abigail,[X3,Y3]]])).
+
+    assert( npc_locations([[julian, [1,2]],[abigail,[X1, Y1]],[nero, [X2, Y2]],[margarette,[X3, Y3]]])).
 
 init_monsters(WS) :-
     retractall( monster_locations(_)),
@@ -91,15 +177,43 @@ init_monsters(WS) :-
     random_between(1, WS, X3), random_between(1, WS, Y3),
     random_between(1, WS, X4), random_between(1, WS, Y4),
 
-    assert( monster_locations([[X1,Y1],[X2,Y2],[X3,Y3],[X4,Y4]])).
+    assert( monster_locations([X1,Y1])),
+    assert( monster_locations([X2,Y2])),
+    assert( monster_locations([X3,Y3])),
+    assert( monster_locations([X4,Y4])).
 
-
-init_world(WS) :-
+init_world(WS, G, CN) :-
     retractall( world_size(_)),
     assert( world_size(WS) ),
-    init_agent,
+%    init_obst(WS),
+
+    retractall(visited(_)),
+    assert(visited(1)),
+
+    retractall(visited_cells(_)),
+    assert(visited_cells([])),
+
+    retractall(trapdoor_location(_)),
+
+    init_agent(G, CN),
     init_npcs(WS),
     init_monsters(WS).
+
+%------------------------------------------------
+% Actions
+
+    visit(Xs) :-
+        visited_cells(Ys),
+        retractall(visited_cells(_)),
+        assert(visited_cells([Ys|Xs])).
+
+%    take_steps(VisitedList) :-
+%%        look_around(Perception),
+%        heroe_location(HL),
+%        npc_locations(NPCL).
+
+
+
 
 %------------------------------------------------
 % Utils
@@ -109,6 +223,8 @@ not_member([X,Y], [[U,V]|Ys]) :-
     ( X=U,Y=V -> fail
     ; not_member([X,Y], Ys)
     ).
+
+
 
 
 
